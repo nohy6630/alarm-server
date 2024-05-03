41883b45d97194fee1ba58cad2e10518fb0c7c59
nohy6630 <nohy6630@gmail.com>
2024-05-03 18:20:37 +0900

    chore: 프로젝트 생성

---
+++ src/test/java/com/yeongjin/alarmserver/AlarmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeongjin.alarmserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
