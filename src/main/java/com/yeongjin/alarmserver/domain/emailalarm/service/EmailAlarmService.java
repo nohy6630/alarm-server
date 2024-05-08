7debba0d58c1481e90264aa6f8b088905b2c06e0
nohy6630 <nohy6630@gmail.com>
2024-05-08 17:28:29 +0900

    feat: 즉시 메일 전송 API 구현

--- src/main/java/com/yeongjin/alarmserver/domain/emailalarm/service/EmailAlarmService.java
+++ src/main/java/com/yeongjin/alarmserver/domain/emailalarm/service/EmailAlarmService.java
@@ -4,15 +4,22 @@ import com.yeongjin.alarmserver.domain.emailalarm.dto.request.ImmediateEmailReq;
 import com.yeongjin.alarmserver.domain.emailalarm.dto.request.ScheduledEmailReq;
 import com.yeongjin.alarmserver.domain.emailalarm.entity.EmailAlarm;
 import com.yeongjin.alarmserver.domain.emailalarm.repository.EmailAlarmRepository;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Slf4j
 public class EmailAlarmService {
     private final EmailAlarmRepository emailAlarmRepository;
+    private final JavaMailSender javaMailSender;
 
     @Transactional
     public Long sendImmediateEmail(ImmediateEmailReq immediateEmailReq) {
@@ -23,6 +30,20 @@ public class EmailAlarmService {
                         immediateEmailReq.getContent()
                 )
         );
+
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+            mimeMessageHelper.setTo(immediateEmailReq.getRecipients().toArray(new String[0]));
+            mimeMessageHelper.setSubject(immediateEmailReq.getSubject());
+            mimeMessageHelper.setText(immediateEmailReq.getContent(), false);
+            javaMailSender.send(mimeMessage);
+            log.info("Success");
+        } catch (MessagingException e) {
+            log.info("Fail");
+            throw new RuntimeException(e);
+        }
+
         return emailAlarm.getId();
     }
 
